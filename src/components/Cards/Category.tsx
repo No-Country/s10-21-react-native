6b5fbe12569bb0a32c238c4734d33fc05bd6cc53
CJB <carlosjbordon@gmail.com>
2023-08-24 01:02:51 -0300

    list icons

--- src/components/Cards/Category.tsx
+++ src/components/Cards/Category.tsx
@@ -4,14 +4,16 @@ import styles from './CategoryStyle';
 
 type Props = {
 	title: string;
-	SvgComponent: React.ReactNode;
+	SvgComponent: any;
 };
 
 export const Category = ({ title, SvgComponent }: Props) => {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.text}>{title}</Text>
-			<View style={styles.svg}>{SvgComponent}</View>
+			<View style={styles.svg}>
+				<SvgComponent width={50} height={50} />
+			</View>
 		</View>
 	);
 };
