cceacc8207ee0fe3d83eb8c5946481212d3f0718
katu <93katu@gmail.com>
2023-08-29 18:56:36 -0300

    cambio de endpoint y buscador funcionando

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,48 +1,46 @@
-import { View, FlatList, Text } from "react-native";
+import React, { lazy, Suspense } from "react";
+import { View, FlatList, ActivityIndicator } from "react-native";
 import SearchHeader from "../../components/searchHeader/SearchHeader";
 import { styles } from "./homeStyles";
 import CardHome from "../../components/cardRecipe/CardRecipe";
-import useSWR from "swr";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { recipeResponseProps } from "../../types/recipeResponse";
-
-
+import { app_id, app_key } from "@env";
 
 const Home = () => {
+  const [query, setQuery] = useState("");
+
+  const getUserQueary = (text: string) => {
+    setQuery(text);
+  };
+
   const [recipes, setRecipes] = useState<recipeResponseProps>();
   const getData = async () => {
     const options = {
-      method: 'GET',
-      url: 'https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2/',
+      method: "GET",
+      url: `https://api.edamam.com/api/recipes/v2`,
       params: {
-        type: 'public',
-        co2EmissionsClass: 'A+',
-        'field[0]': 'uri',
-        beta: 'true',
-        random: 'true',
-        'cuisineType[0]': 'American',
-        'imageSize[0]': 'LARGE',
-        'mealType[0]': 'Breakfast',
-        'health[0]': 'alcohol-cocktail',
-        'diet[0]': 'balanced',
-        'dishType[0]': 'Biscuits and cookies',
-        from: 20,
-        to: 30
+        type: "public",
+        app_id: app_id,
+        app_key: app_key,
+        beta: "true",
+        random: "true",
+        diet: "low-carb",
+        healt: "alcohol-free",
+        cuisineType: "American",
+        q: query,
       },
-
       headers: {
-        'Accept-Language': 'en',
-        'X-RapidAPI-Key': '88a864ae53msh999934775aef7fcp1fd42djsn78b996068f97',
-        'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-      }
+        "Accept-Language": "en",
+      },
     };
 
     try {
       const { data } = await axios.request<recipeResponseProps>(options);
       setRecipes(data);
     } catch (error) {
-      console.warn(error);
+      console.warn(error.message);
     }
   };
 
@@ -52,32 +50,36 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <SearchHeader />
+      <SearchHeader getUserQueary={getUserQueary} query={query} />
 
-      <FlatList
-        contentContainerStyle={{
-          rowGap: 20,
-          justifyContent: "space-between",
-        }}
-        columnWrapperStyle={{
-          justifyContent: "space-between",
-          paddingHorizontal: 3,
-        }}
-        data={recipes?.hits}
-        renderItem={({ item }) => {
-          return (
-            <CardHome
-              calories={item.recipe.calories}
-              tittle={item.recipe.label}
-              totalTime={item.recipe.totalTime}
-              imageUrl={item.recipe.image}
-            />
-          );
-        }}
-        keyExtractor={(item) => item?.recipe?.uri}
-        numColumns={2}
-        horizontal={false}
-      />
+      {recipes?.hits.length > 0 ? (
+        <FlatList
+          contentContainerStyle={{
+            rowGap: 20,
+            justifyContent: "space-between",
+          }}
+          columnWrapperStyle={{
+            justifyContent: "space-between",
+            paddingHorizontal: 3,
+          }}
+          data={recipes?.hits}
+          renderItem={({ item }) => {
+            return (
+              <CardHome
+                calories={item.recipe.calories}
+                tittle={item.recipe.label}
+                totalTime={item.recipe.totalTime}
+                imageUrl={item.recipe.image}
+              />
+            );
+          }}
+          keyExtractor={(item) => item?.recipe?.uri}
+          numColumns={2}
+          horizontal={false}
+        />
+      ) : (
+        <ActivityIndicator size="large" />
+      )}
     </View>
   );
 };
