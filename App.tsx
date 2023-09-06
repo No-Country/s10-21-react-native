16a4950e63bdb4902eaa49e95168ae3e065b72cc
FranBaca <franbaca13@gmai.com>
2023-09-06 17:35:15 -0300

    New google SignIn in the works

--- App.tsx
+++ App.tsx
@@ -3,13 +3,25 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootStack from './src/navigator/RootStack';
 import { StatusBar } from 'expo-status-bar';
 import AuthScreen from './src/screens/AuthScreen/AuthScreen';
-
-
+import RegistrationScreen from './src/components/Auth/RegistrationScreen';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+const Stack = createStackNavigator()
 
 export default function App() {
 		return (
 		<SafeAreaProvider>
-			<RootStack />
+			  <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >    
+          <Stack.Screen name="RegisterScreen" component={AuthScreen} />
+		  
+        </Stack.Navigator>
+      </NavigationContainer>
 		</SafeAreaProvider>
 	);
 }
--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,8 +1,12 @@
 
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, ScrollView} from 'react-native';
 import * as WebBrowser from "expo-web-browser";
 import { useState, useEffect } from 'react';
-import * as Google from "expo-auth-session/providers/google"
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+} from '@react-native-google-signin/google-signin';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 
@@ -11,77 +15,55 @@ WebBrowser.maybeCompleteAuthSession();
 
 export default function Auth() {
 	const [user, setUser] = useState(null)
-	const[request, response , promptAsync] = Google.useAuthRequest({
-		androidClientId:"197093024995-ohnc4tcbqu3v99g36kursr9bapv59ok2.apps.googleusercontent.com",
-		webClientId:"197093024995-2ji9cs8uvgj4119vrjahvppep8snrvga.apps.googleusercontent.com"
-	});
+		//androidClientId:"197093024995-ohnc4tcbqu3v99g36kursr9bapv59ok2.apps.googleusercontent.com",
+		//webClientId:"197093024995-2ji9cs8uvgj4119vrjahvppep8snrvga.apps.googleusercontent.com"
 
-	const getLocalUser = async () => {
-		const data = await AsyncStorage.getItem("@user");
-		if(!data) return null
-		return JSON.parse(data)
-	}
+    useEffect(() => {
+      GoogleSignin.configure({
+        webClientId:"197093024995-2ji9cs8uvgj4119vrjahvppep8snrvga.apps.googleusercontent.com"
+      })
+    },[])
 
-	const getUserInfo = async (token) => {
-		if(!token) return;
+    const signIn = async () =>{
+      try {
+        await GoogleSignin.hasPlayServices();
+        const userInfo = await GoogleSignin.signIn();
+        setUser({ userInfo });
+      } catch (error) {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          // operation (e.g. sign in) is in progress already
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          // play services not available or outdated
+        } else {
+          // some other error happened
+        }
+      }
+    }
 
-		try{
-			const response = await fetch(
-				"https://www.googleapis.com/userinfo/v2/me",{
-					headers: {Authorization: `Bearer ${token}`}
-				}
-			);
-			const user = await response.json()
-			await AsyncStorage.setItem("@user", JSON.stringify(user))
-			setUser(user)
-		}catch(e){
-			console.log(e)
-		}
-	}
+    const signOut = async () => {
+      try {
+        await GoogleSignin.signOut();
+        setUser({ user: null }); // Remember to remove the user from your app's state as well
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-	useEffect(() => {
-		handleSignInWithGoogle()
-	}, [response])
-
-	async function handleSignInWithGoogle(){
-		const user = await getLocalUser();
-		if(!user){
-			if(response?.type === "success"){
-				getUserInfo(response.authentication.accessToken)
-			}else{
-				setUser(user)
-			}
-		}
-	}
     return (
-	<View style={styles.container}>
-      {!user ? (
-        <TouchableOpacity style={styles.MainButtons} disabled={!request} onPress={() => {promptAsync();}}>
-            <Image
-            source={require("../../../assets/icons/google.png")}
-            style={styles.buttonImageIconStyle}
-            />
-            <Text style={styles.text}>Sign In With Google</Text>
-        </TouchableOpacity>
-      ) : (
-        <View style={styles.container}>
-          {user?.picture && (
-            <Image source={{ uri: user?.picture }} style={styles.image} />
-          )}
-          <Text style={styles.text}>Email: {user.email}</Text>
-          <Text style={styles.text}>
-            Verified: {user.verified_email ? "yes" : "no"}
-          </Text>
-          <Text style={styles.text}>Name: {user.name}</Text>
-          {/* <Text style={styles.text}>{JSON.stringify(userInfo, null, 2)}</Text> */}
-        </View>
-      )}
-      <TouchableOpacity style={styles.MainButtons} onPress={async () => await AsyncStorage.removeItem("@user")}>
-        <BsFillPersonLinesFill color='white'/>
-            <Text style={styles.text}>Enter as guest</Text>
-      </TouchableOpacity>
-
-    </View>
+		<View>
+			<ScrollView contentInsetAdjustmentBehavior="automatic">        
+            <View style={styles.container}>
+              <GoogleSigninButton
+                style={{width: 192, height: 48}}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={this._signIn}
+              />
+            </View>
+        </ScrollView>
+		</View>
 	);
 }
 
