119964abe9333f1b6612df8d33b94765638ddf75
CJB <carlosjbordon@gmail.com>
2023-09-12 20:12:33 -0300

    recipe endpoints

--- backend/controllers/recipes.ts
+++ backend/controllers/recipes.ts
@@ -31,3 +31,81 @@ export const createRecipe = async (req: Request, res: Response) => {
 		});
 	}
 };
+
+export const getRecipes = async (req: Request, res: Response) => {
+	try {
+		const recipes = await Recipe.find().exec();
+		res.json({ msg: 'Get recipes', recipes });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'Error' + error,
+		});
+	}
+};
+
+export const getRecipe = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	try {
+		const recipe = await Recipe.findById(id).exec();
+
+		if (!recipe) {
+			return res.status(404).json({
+				msg: 'Recipe not found',
+			});
+		}
+
+		res.json({ msg: 'Get recipe', recipe });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'Error' + error,
+		});
+	}
+};
+
+export const updateRecipe = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const { _id, ...recipe } = req.body;
+
+	try {
+		const recipeUpdated = await Recipe.findByIdAndUpdate(id, recipe, {
+			new: true,
+		}).exec();
+
+		if (!recipeUpdated) {
+			return res.status(404).json({
+				msg: 'Recipe not found',
+			});
+		}
+
+		res.json({ msg: 'Update recipe', recipeUpdated });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'Error' + error,
+		});
+	}
+};
+
+export const deleteRecipe = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	try {
+		const recipe = await Recipe.findByIdAndDelete(id).exec();
+
+		if (!recipe) {
+			return res.status(404).json({
+				msg: 'Recipe not found',
+			});
+		}
+
+		res.json({ msg: 'Delete recipe', recipe });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			msg: 'Error' + error,
+		});
+	}
+};
